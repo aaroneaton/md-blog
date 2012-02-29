c56e1e1b885ff5f7035cbc93361003056fd4e778
Aaron Eaton <aaron@channeleaton.com>
2012-02-29 17:15:54 -0600

    Can now view single posts

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,8 +3,17 @@
 	<div class="span12">
 	
 	<?php foreach ( $posts as $post ) : ?>
-	<h1><?php echo $post['title']; ?></h1>
+  <h1><?php echo anchor( $post['file'], $post['title'] ); ?></h1>
+  <h6><?php echo $post['date']; ?></h6>
 	<?php echo $post['content']; ?>
+<h6>Tags: </h6>
+  <p><?php foreach ( $post['tags'] as $tag ) : ?>
+  
+<?php echo anchor( 'tag/' . $tag, $tag ) . ' '; ?>
+  
+<?php endforeach; ?>
+  </p>
+<hr />
 	<?php endforeach; ?>	
 	</div>
 </div>
