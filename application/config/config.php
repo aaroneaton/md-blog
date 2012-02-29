5d9dfb9a485434261f78c0bfdcdbcfbdaeeec7ac
Aaron Eaton <aaron@channeleaton.com>
2012-02-29 10:06:31 -0600

    Base URL problems

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/md-blog/';
+$config['base_url']	= 'http://local.localhost/md-blog/';
 
 /*
 |--------------------------------------------------------------------------
